0cccc3e525294b3d1f9f5e28880b1ae7709e2b9a
4169AI <xiaowei88vx@gmail.com>
2026-06-10 14:32:35 +0800

    第一天基础练习，学习注释，input函数，变量赋值，print打印

--- day01.py
+++ day01.py
@@ -1,1 +1,17 @@
 print('推送 Git 和 Python')
+print("我是嘉伟，今天开始学习 python")
+a = 10
+b = 20
+c = 1.75
+d = 1.1
+
+print(a+b,a*b,a-b,a/b,a+c,a*c,a/c)
+
+name = "嘉伟"
+age = 18
+height = "75千克"
+print("你的名字 你的年龄 你的体重",name,age,height)
+
+#使用input()让用户输入名字
+name = input("请输入你的名字：")
+print("你好"+name+"欢迎学习python")
